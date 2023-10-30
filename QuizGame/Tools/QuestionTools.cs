b5ba240f16fc697e34c7b88e75e8c96a779a3ec8
Lutek <lucianocoddingstuff@gmail.com>
2023-10-30 19:08:30 +0100

    Initialization of basic classes

---
+++ QuizGame/Tools/QuestionTools.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Tools
+{
+    public class QuestionTools
+    {
+        #region Public Fields
+        #endregion
+
+        #region Private Fields
+        #endregion
+
+        #region ctors
+        public QuestionTools() { }
+        #endregion
+
+        #region Public Methods
+/*
+        public List<Question> loadQuestions()
+        {
+            
+        }
+*/
+        public void addQuestion()
+        {
+
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+    }
+}
--- QuizGame/Views/Home.cs
+++ QuizGame/Views/Home.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace QuizGame
+namespace QuizGame.Views
 {
-    public partial class Form1 : Form
+    public partial class Home : Form
     {
-        public Form1()
+        public Home()
         {
             InitializeComponent();
         }
